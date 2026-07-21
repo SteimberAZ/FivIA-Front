8cb119f2efdb34f0ad18562de7caba633d030d55
Steimber <randyarteaga1519@gmail.com>
2026-07-21 17:03:20 -0500

    Agregadas credenciales de Supabase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'TU_SUPABASE_URL',
-  supabaseKey: 'TU_SUPABASE_ANON_KEY',
+  supabaseUrl: 'https://pytzthgogluigkzbpmaj.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5dHp0aGdvZ2x1aWdremJwbWFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2NTk5NzQsImV4cCI6MjEwMDIzNTk3NH0.1F1CpXhrxKUfRnnurdPLipewSFUJPP-6wCuxvxJd7OU',
   apiUrl: 'https://fivia-backend.vercel.app/api'
 };
